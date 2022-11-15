fd64c3177206ec06ecfb29672a9f8be61ac82348
lcomment <kohyunsuk98@gmail.com>
2022-11-15 23:17:41 +0900

    test: 카페 주소를 추가한 카페 등록 테스트 작성

--- src/test/java/net/cafree/domain/cafe/dao/CafeRepositoryTest.java
+++ src/test/java/net/cafree/domain/cafe/dao/CafeRepositoryTest.java
@@ -1,6 +1,7 @@
 package net.cafree.domain.cafe.dao;
 
 import net.cafree.domain.cafe.domain.Cafe;
+import net.cafree.domain.cafe.domain.CafeAddress;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,6 +21,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CafeRepositoryTest {
     @Autowired
     CafeRepository cafeRepository;
+
+    @Autowired
+    CafeAddressRepository cafeAddressRepository;
 
     @AfterEach
     public void cleanup(){
@@ -28,20 +34,58 @@ public class CafeRepositoryTest {
     @Test
     public void registerNewCafe(){
         // given
-        String title = "title_example";
-        String naver_url = "naver_url_example";
+        String sido = "경기";
+        String sigungu = "안양시 만안구";
+        String dong = "안양동";
+        String doro = "장내로149번길 53";
+        String build_no = "674-81";
+        String branch = "안양역점";
+        BigDecimal latitude = BigDecimal.valueOf(126.922145);
+        BigDecimal longitude = BigDecimal.valueOf(37.4002960);
 
-        cafeRepository.save(Cafe.builder()
+        CafeAddress cafeAddress = CafeAddress.builder()
+                        .sido(sido)
+                        .sigungu(sigungu)
+                        .dong(dong)
+                        .doro(doro)
+                        .build_no(build_no)
+                        .branch(branch)
+                        .latitude(latitude)
+                        .longitude(longitude)
+                        .build();
+
+        String title = "스타벅스";
+        String naver_url = "https://map.naver.com/v5/search/스타벅스%20안양일번가/place/37169041?placePath=%3Fentry=pll%26from=nx%26fromNxList=true";
+
+        Cafe cafe = Cafe.builder()
                 .title(title)
                 .naver_url(naver_url)
-                .build());
+                .cafeAddress(cafeAddress)
+                .build();
+
+        cafeRepository.save(cafe);
+        cafeAddressRepository.save(cafeAddress);
 
         // when
-        List<Cafe> cafeList = cafeRepository.findAll();
+        Cafe existCafe = cafeRepository.findById(cafe.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 카페가 존재하지 않습니다"));
+        CafeAddress existCafeAddress = cafeAddressRepository.findById(cafeAddress.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 주소가 존재하지 않습니다"));
 
         // then
-        Cafe cafe = cafeList.get(0);
-        assertThat(cafe.getTitle()).isEqualTo(title);
-        assertThat(cafe.getNaver_url()).isEqualTo(naver_url);
+        assertThat(existCafe.getTitle()).isEqualTo(title);
+        assertThat(existCafe.getNaver_url()).isEqualTo(naver_url);
+
+        CafeAddress joinAddress = existCafe.getCafeAddress();
+        assertThat(existCafeAddress.getId()).isEqualTo(joinAddress.getId());
+        assertThat(existCafeAddress.getSido()).isEqualTo(joinAddress.getSido());
+        assertThat(existCafeAddress.getSigungu()).isEqualTo(joinAddress.getSigungu());
+        assertThat(existCafeAddress.getEupmyun()).isEqualTo(joinAddress.getEupmyun());
+        assertThat(existCafeAddress.getDoro()).isEqualTo(joinAddress.getDoro());
+        assertThat(existCafeAddress.getDong()).isEqualTo(joinAddress.getDong());
+        assertThat(existCafeAddress.getBuild_no()).isEqualTo(joinAddress.getBuild_no());
+        assertThat(existCafeAddress.getBranch()).isEqualTo(joinAddress.getBranch());
+        assertThat(existCafeAddress.getLatitude()).isEqualTo(joinAddress.getLatitude());
+        assertThat(existCafeAddress.getLongitude()).isEqualTo(joinAddress.getLongitude());
     }
 }
