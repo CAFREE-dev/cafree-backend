1ef4d489479237f331a7b865d620aefabc5e37c1
lcomment <kohyunsuk98@gmail.com>
2022-12-02 15:35:37 +0900

    feature: JpaRepository 쿼리 메서드 추가

--- src/main/java/net/cafree/domain/feed/repository/TagRepository.java
+++ src/main/java/net/cafree/domain/feed/repository/TagRepository.java
@@ -3,5 +3,13 @@ package net.cafree.domain.feed.repository;
 import net.cafree.domain.feed.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    /* 2022.11.30 lcomment : Tag 이름으로 존재하는 Tag 엔티티 반환 */
+    Tag findByTagName(String tagName);
+
+    /* 2022.12.01 lcommet :  IN 쿼리 */
+    List<Tag> findByIdIn(List<Long> ids);
 }
